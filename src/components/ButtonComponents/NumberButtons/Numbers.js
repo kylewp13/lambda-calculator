f76e7f5c4b4c15e6b2ce6db2b237d26c39a057ef
Thomas Hallberg <thomashallberg@Thomass-Air.attlocal.net>
2019-11-05 17:19:03 -0500

    added useState and imports for Operators

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -3,16 +3,13 @@ import NumberButton from "./NumberButton";
 import numbers from "../../../data";
 
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
+
 //Import your array data to from the provided data file
 
 const Numbers = (props) => {
   // STEP 2 - add the imported data to state
   const [numberState, setNumberState] = useState(numbers);
-  console.log(numberState.numbers);
+  // console.log(numberState);
   return (
     <div className="wrapper-numbers">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import OperatorButton from "./OperatorButton";
+import operators from "../../../data";
 
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Operators = () => {
+const Operators = (props) => {
   // STEP 2 - add the imported data to state
+  const [operatorState, setOperatorState] = useState(operators)
+  // console.log(operatorState.operators)
   return (
-    <div>
+    <div className="wrapper-numbers">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+
+      operatorState.operators.map((button, index) => (
+        <OperatorButton key={index} button={button.char} />
+      ))}
     </div>
   );
 };
+
+export default Operators
